09806eadba614f457486caae8aac232af762e321
Evandro Galo Vieira <evandrogv123@gmail.com>
2023-09-20 18:16:59 -0300

    Alterações na Navbar e tela de Cadastro de casa de aposta
    Mudanças na Navbar (Adição de "Lista de Cadastros", mudança de cor dos botões)
     Alterado terceira caixa de texto em "Cadastro de casa de aposta" para "Imagem" do tipo "URL".

---
+++ src/components/Navbar.vue
@@ -0,0 +1,96 @@
+<template>
+    <header>
+        <div class="navbars">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+        <div class="dropdown">
+
+          <button class="dropbtn">Conta</button>
+             <div class="dropdown-content">
+              <a href="/cadastro">Cadastro de usuário</a>
+              <a href="/cadastro-casa-de-aposta">Cadastro de casa de aposta</a>
+      </div>
+
+    </div>
+     <div class="lista_sites_cadastrados">
+      <nav>
+      <RouterLink to="/lista_sites_cadastrados">Lista de Cadastros</RouterLink>
+      </nav>
+     </div>
+
+    </div>
+
+    </header>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Mukta&family=Quicksand:wght@600&family=Rubik&display=swap');
+.navbars {
+  background-color: transparent;
+  overflow: hidden;
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  font-weight: normal;
+  font-family: 'Rubik', sans-serif;
+}
+.navbars a {
+  float: left;
+  color: #4ef3ff;;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 18px;
+}
+
+.navbars a:hover {
+  background-color: hsla(160, 100%, 37%, 0.2);
+  color: #4ef3ff;
+}
+
+.dropbtn {
+  background-color: transparent;
+  overflow: hidden;
+  color:#4ef3ff;
+  padding: 2rem;
+  cursor: pointer;
+  border: none;
+  outline: none;
+  font-family: 'Rubik', sans-serif;
+  font-size: 18px;
+}
+
+.dropdown {
+  display: block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #FAF9F6;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  font-family: 'Rubik', sans-serif;
+  font-size: 13px;
+}
+
+.dropdown-content a:hover {background-color: hsla(160, 100%, 37%, 0.2)}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+
+</style>
---
+++ src/views/Cadastro_aposta.vue
@@ -0,0 +1,127 @@
+<template>
+
+    <div id="cadastro">
+          <form  action="">
+              <h2 class="titulo">CADASTRE-SE</h2>
+              <div class="input1">
+                  <input id="nome" name="nome" placeholder="Nome" type="text">
+              </div>
+              <div class="input1">
+                  <input id="link" name="link" placeholder="Link" type="url">
+              </div>
+              <div class="input1">
+                  <input id="imagem" name="imagem" placeholder="Imagem" type="url">
+              </div>
+              <div id="button">
+                  <button type="submit">CONFIRMAR</button>
+              </div>
+          </form>
+      </div>
+    
+    </template>
+    
+    <script>
+    
+    </script>
+    
+    <style scoped>
+        @import url('https://fonts.googleapis.com/css2?family=Mukta&family=Quicksand:wght@600&family=Rubik&display=swap');
+    
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    
+    body{
+        width: 100%;
+        height: 100vh;
+        font-family: 'Mukta', sans-serif;
+        background-color: #adaaa1;
+    }
+    
+    #cadastro{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background: #FAF9F6;
+        border-radius: 10px;
+        padding: 60px;
+        width: min-content;
+        align-items: center;
+        color: #1BCC23;
+    }
+    
+    .id{
+        min-width: 200px;
+        border-radius: 100px;
+        color: #000000;
+        padding: 5px;
+        margin: 10px 0px 12px -5px;
+    }
+
+    .titulo{
+        font-size: 0.7cm;
+        margin-bottom: 5%;
+    }
+    
+    .input1{
+        min-width: 200px;
+        border-radius: 100px;
+        border: 2px #1BCC23 solid;
+        padding: 5px;
+        margin: 10px 0px 12px -5px;
+    }
+    
+    .input1 input{
+        width: 88%;
+        border: none;
+        outline: none;
+        font-family: 'Mukta', sans-serif;
+        transform: translateX(5%);
+        color: #000000;
+        font-size: 15px;
+    }
+    
+    .input{
+        min-width: 250px;
+        border-radius: 100px;
+        border: 2px #1BCC23 solid;
+        padding: 6px;
+        margin: 10px 0px 12px -5px;
+    }
+    
+    .input input{
+        width: 88%;
+        border: none;
+        outline: none;
+        font-family: 'Mukta', sans-serif;
+        transform: translateX(5%);
+        color: #000000;
+        font-size: 15px;
+    }
+    
+    #button{
+        margin-top: 20px;
+        text-align: center;
+    }
+    
+    #button button{
+        font-family: 'Mukta', sans-serif;
+        background: #1BCC23;
+        width: 40%;
+        height: 30px;
+        border-radius: 100px;
+        border: none;
+        font-size: 16px;
+        color: #FAF9F6;
+        outline: none;
+        cursor: pointer;
+    }
+    
+    #button button:hover{
+        background: #149b1b;
+    }
+    
+
+    </style>
